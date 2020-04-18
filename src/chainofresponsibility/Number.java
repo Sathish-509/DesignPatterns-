75d90c43f395493808563e34b1e2bab33d024787
Sathish <sathish9yerramsetti@gmail.com>
2020-04-18 18:18:18 +0800

    chain ofresponsibility added

---
+++ src/chainofresponsibility/Number.java
@@ -0,0 +1,17 @@
+package chainofresponsibility;
+
+class Number  
+{  
+    private int number;  
+  
+    public Number(int number)  
+    {  
+        this.number = number;  
+    }  
+  
+    public int getNumber()  
+    {  
+        return number;  
+    }  
+  
+}
---
+++ src/chainofresponsibility/PositiveProcessor.java
@@ -0,0 +1,21 @@
+package chainofresponsibility;
+
+class PositiveProcessor extends Processor  
+{  
+  
+    public PositiveProcessor(Processor processor){ 
+        super(processor); 
+    } 
+  
+    public void process(Number request)  
+    {  
+        if (request.getNumber() > 0)  
+        {  
+            System.out.println("PositiveProcessor : " + request.getNumber());  
+        }  
+        else
+        {  
+            super.process(request);  
+        }  
+    }  
+}
